f2fb92e4ada6b0b5f3f2b880a861756ebc6cca1a
Basil Alasadi <8195420+generic19@users.noreply.github.com>
2025-04-23 11:11:12 +0300

    Created: Tasks Repository

---
+++ Task Organizer/TasksRepository.h
@@ -0,0 +1,25 @@
+//
+//  TasksRepository.h
+//  Task Organizer
+//
+//  Created by Basel Alasadi on 23/04/2025.
+//
+
+#import <Foundation/Foundation.h>
+#import "Task.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TasksRepository : NSObject
+
++ (instancetype)repositoryWithUserDefaults:(NSUserDefaults*)userDefaults;
+
+@property (readonly) NSArray<Task*>* tasks;
+
+- (void)addTask:(Task*)task;
+- (void)removeTask:(Task*)task;
+- (void)replaceTask:(Task*)oldTask with:(Task*)task;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Task Organizer/TasksRepository.m
@@ -0,0 +1,68 @@
+//
+//  TasksRepository.m
+//  Task Organizer
+//
+//  Created by Basel Alasadi on 23/04/2025.
+//
+
+#import "TasksRepository.h"
+
+@interface TasksRepository ()
+
+@property (readwrite) NSMutableArray<Task*>* tasks;
+
+@end
+
+@implementation TasksRepository {
+    NSUserDefaults* defaults;
+}
+
++ (nonnull instancetype)repositoryWithUserDefaults:(nonnull NSUserDefaults *)userDefaults {
+    TasksRepository* repo = [[TasksRepository alloc] init];
+    
+    if (repo != nil) {
+        repo->defaults = userDefaults;
+        [repo loadTasks];
+    }
+    
+    return repo;
+}
+
+- (void)loadTasks {
+    NSData* data = [defaults objectForKey:@"tasks"];
+    NSArray* unarchived = [NSKeyedUnarchiver unarchivedArrayOfObjectsOfClass:[Task class] fromData:data error:nil];
+    
+    if (unarchived == nil) {
+        _tasks = [NSMutableArray array];
+    } else {
+        _tasks = [NSMutableArray arrayWithArray:unarchived];
+    }
+}
+
+- (void)saveTasks {
+    NSData* data = [NSKeyedArchiver archivedDataWithRootObject:_tasks requiringSecureCoding:NO error:nil];
+    if (data != nil) {
+        [defaults setObject:data forKey:@"tasks"];
+    }
+}
+
+- (void)addTask:(nonnull Task *)task {
+    [_tasks addObject:task];
+    [self saveTasks];
+}
+
+- (void)removeTask:(nonnull Task *)task {
+    [_tasks removeObject:task];
+    [self saveTasks];
+}
+
+- (void)replaceTask:(nonnull Task *)oldTask with:(nonnull Task *)task {
+    NSUInteger index = [_tasks indexOfObject:oldTask];
+    
+    if (index != NSNotFound) {
+        _tasks[index] = task;
+        [self saveTasks];
+    }
+}
+
+@end
